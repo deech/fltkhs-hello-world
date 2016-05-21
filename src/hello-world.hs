2bef32883f122e88e8a9bc63f9c90eae0d06b7c7
deech <aditya.siram@gmail.com>
2016-05-21 16:56:26 -0500

    Revert changes to demo.

--- src/hello-world.hs
+++ src/hello-world.hs
@@ -1,25 +1,29 @@
 module Main where
 import qualified Graphics.UI.FLTK.LowLevel.FL as FL
 import Graphics.UI.FLTK.LowLevel.Fl_Types
-import Graphics.UI.FLTK.LowLevel.Fl_Enumerations
 import Graphics.UI.FLTK.LowLevel.FLTKHS
+
+
+buttonCb :: Ref Button -> IO ()
+buttonCb b' = do
+  l' <- getLabel b'
+  if (l' == "Hello world")
+    then setLabel b' "Goodbye world"
+    else setLabel b' "Hello world"
 
 main :: IO ()
 main = do
-    w <- windowNew (toSize (100, 100)) Nothing Nothing
-    let funcs = defaultCustomWidgetFuncs {handleCustom = Just handleEv}
-    _ <- buttonCustom
-            (toRectangle (30, 30, 40, 40))
-            Nothing Nothing (Just funcs)
-    end w
-    showWidget w
-    _ <- FL.run
-    return ()
-
-drawF :: Ref Button -> IO ()
-drawF = drawSuper
-
-handleEv :: Ref Button -> Event -> IO Int
-handleEv w ev = do
-    print ev
-    handleSuper w ev
+ window <- windowNew
+           (Size (Width 115) (Height 100))
+           Nothing
+           Nothing
+ begin window
+ b' <- buttonNew
+        (Rectangle (Position (X 10) (Y 30)) (Size (Width 95) (Height 30)))
+        (Just "Hello world")
+ setLabelsize b' (FontSize 10)
+ setCallback b' buttonCb
+ end window
+ showWidget window
+ _ <- FL.run
+ return ()
