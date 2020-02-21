4ddf3ce226fa190a0e4804e716fc816b9404e470
Aditya Siram <aditya.siram@gmail.com>
2020-02-21 11:55:00 -0600

    Bump FLTKHS version to 0.8.0.3, minimum Cabal to 2.4.0.0 and upgrade Stackage to lts-15.0

--- Setup.hs
+++ Setup.hs
@@ -2,6 +2,7 @@
 module Main (main) where
 
 import Distribution.Simple
+import Distribution.Simple.Setup
 import Distribution.Simple.Program
 import Distribution.Simple.LocalBuildInfo
 import Distribution.PackageDescription
@@ -15,6 +16,7 @@ import Distribution.System (buildOS, OS(Windows, OSX))
 import Distribution.InstalledPackageInfo
 import System.IO.Error
 
+_PackageName :: String -> PackageName
 #if defined(MIN_VERSION_Cabal) && MIN_VERSION_Cabal(2,0,0)
 _PackageName = mkPackageName
 #else
@@ -24,14 +26,14 @@ _PackageName = PackageName
 main :: IO ()
 main = defaultMainWithHooks (simpleUserHooks { buildHook = myBuildHook })
 
+myBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
 myBuildHook pkg_descr local_bld_info user_hooks bld_flags =
-  let fltkhsDependency = lookupDependency (installedPkgs local_bld_info) (Dependency (_PackageName "fltkhs") anyVersion)
+  let fltkhsDependency = lookupDependency (installedPkgs local_bld_info) (_PackageName "fltkhs") anyVersion
       keepBuilding = (buildHook simpleUserHooks) pkg_descr local_bld_info user_hooks bld_flags
   in
-  case fltkhsDependency of
+  case fmap snd fltkhsDependency of
     [] -> keepBuilding
-    (_,[]):_ -> keepBuilding
-    (_, (packageInfo:_)):_ ->
+    (packageInfo:_):_ ->
        mapM_ (updateEnv "LIBRARY_PATH") (libraryDirs packageInfo) >>
        keepBuilding
 
